5688e60d8d5c189e446ff3f51ce967979fd861d2
stevex2012 <18725873459@163.com>
2021-11-08 11:39:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+// 判空
+export const isEmpty = /^[\s\S]*.*[^\s][\s\S]*$/g;
+// 请求路径，网站地址
+export const isUrl = /^(https?:\/\/(([a-zA-Z0-9]+-?)+[a-zA-Z0-9]+\.)+[a-zA-Z]+)(:\d+)?(\/.*)?(\?.*)?(#.*)?$/;
