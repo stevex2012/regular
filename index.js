af67ed9488f9c4ffdb46b9653eb507c97754969b
stevex2012 <18725873459@163.com>
2022-09-15 10:29:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 export const isEmpty = /^[\s\S]*.*[^\s][\s\S]*$/g;
 // 请求路径，网站地址
 export const isUrl = /^(https?:\/\/(([a-zA-Z0-9]+-?)+[a-zA-Z0-9]+\.)+[a-zA-Z]+)(:\d+)?(\/.*)?(\?.*)?(#.*)?$/;
+// 邮箱
+export const emailRegx =
+  /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/;
+// 手机号码
+export const mobileRegx = /^1[3456789]\d{9}$/;
+// 身份证
+export const regIdCard1 = /^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/;
+
+export const regIdCard2 = /^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$/;
